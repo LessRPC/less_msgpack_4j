1e38cdc2cfc0c62721672045ab9e3776d0abfd0e
salimm <salimm@cs.pitt.edu>
2018-01-26 13:46:58 -0500

    added stable version 1

---
+++ src/org/lessrpc/serialize/msgpack/MsgPackSerializer.java
@@ -0,0 +1,203 @@
+package org.lessrpc.serialize.msgpack;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.lessrpc.common.info.EnvironmentInfo;
+import org.lessrpc.common.info.SerializationFormat;
+import org.lessrpc.common.info.ServiceInfo;
+import org.lessrpc.common.info.ServiceLocator;
+import org.lessrpc.common.info.ServiceRequest;
+import org.lessrpc.common.info.responses.ExecuteRequestResponse;
+import org.lessrpc.common.info.responses.ServiceResponse;
+import org.lessrpc.common.serializer.Serializer;
+
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.databind.Module;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class MsgPackSerializer extends Serializer {
+
+	/**
+	 * mapper pointer
+	 */
+	private ObjectMapper mapper;
+
+	public MsgPackSerializer() {
+		mapper = new ObjectMapper(new JsonFactory());
+	}
+
+	public MsgPackSerializer(ObjectMapper mapper) {
+		this.mapper = mapper;
+	}
+
+	@Override
+	public <T> byte[] serialize(T obj, Class<T> cls) throws Exception {
+		return mapper.writeValueAsBytes(obj);
+	}
+
+	@Override
+	public <T> T deserialize(byte[] data, Class<T> cls) throws Exception {
+		return mapper.readValue(data, cls);
+	}
+
+	@Override
+	public <T> void serialize(T obj, Class<T> cls, OutputStream os) throws Exception {
+		mapper.writeValue(os, obj);
+	}
+
+	@Override
+	public <T> T deserialize(InputStream is, Class<T> cls) throws Exception {
+		return mapper.readValue(is, cls);
+	}
+
+	@Override
+	public Serializer prepare(String schema) {
+		return this;
+	}
+
+	@Override
+	public Serializer prepare(Module module) {
+		ObjectMapper map = mapper.copy();
+		map.registerModule(module);
+
+		return new MsgPackSerializer(map);
+	}
+
+	@Override
+	public Serializer prepare(Module module, String schema) {
+		ObjectMapper map = mapper.copy();
+		map.registerModule(module);
+
+		return new MsgPackSerializer(map);
+	}
+
+	@Override
+	public SerializationFormat getType() {
+		return new SerializationFormat("MSGPACK", "2.0");
+	}
+
+	public Serializer copy() {
+		return new MsgPackSerializer(mapper.copy());
+	}
+
+	@Override
+	public <T, S> T deserialize(byte[] data, Class<T> cls, ServiceLocator locator) throws Exception {
+		ByteArrayInputStream in = new ByteArrayInputStream(data);
+		return deserialize(in, cls, locator);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public <T, S> T deserialize(InputStream in, Class<T> cls, ServiceLocator locator) throws Exception {
+		if (cls.equals(ServiceRequest.class)) {
+			return (T) parseServiceRequest(in, locator);
+		} else if (cls.equals(ExecuteRequestResponse.class)) {
+			return (T) parseExecuteRequestResponse(in, locator);
+		} else {
+			throw new Error(
+					"This function can only be used for ServiceRequest and  ExecuteRequestResponse but received: "
+							+ cls);
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	private ExecuteRequestResponse<?> parseExecuteRequestResponse(InputStream in, ServiceLocator locator)
+			throws JsonParseException, IOException {
+		JsonParser parser = mapper.getFactory().createParser(in);
+		ExecuteRequestResponse<?> response = new ExecuteRequestResponse<>();
+
+		JsonToken token = null;
+		String fieldName = null;
+		while ((token = parser.nextToken()) != null) {
+			if (token == JsonToken.FIELD_NAME) {
+				fieldName = parser.getValueAsString();
+			} else if (fieldName != null) {
+				if (fieldName.equals("status")) {
+					int status = parser.getIntValue();
+					response.setStatus(status);
+				} else if (fieldName.equals("content")) {
+					@SuppressWarnings("rawtypes")
+					ServiceResponse res = new ServiceResponse<>();
+					String fieldName2 = null;
+					while ((token = parser.nextToken()) != JsonToken.END_OBJECT) {
+						if (token == JsonToken.FIELD_NAME) {
+							fieldName2 = parser.getValueAsString();
+						} else if (fieldName2 != null) {
+							if (fieldName2.equals("rid")) {
+								long rid = parser.getLongValue();
+								res.setRequestId(rid);
+							} else if (fieldName2.equals("service")) {
+								ServiceInfo<?> service = parser.readValueAs(ServiceInfo.class);
+								res.setService(service);
+							} else if (fieldName2.equals("content")) {
+								Object obj = parser
+										.readValueAs(locator.locate(res.getService().getId()).getResultClspath());
+								res.setObject(obj);
+							}
+							fieldName2 = null;
+						}
+
+					}
+					response.setContent(res);
+				} else {
+					throw new Error("Unkown Field");
+				}
+				fieldName = null;
+			}
+
+		}
+
+		return response;
+
+	}
+
+	private ServiceRequest parseServiceRequest(InputStream in, ServiceLocator locator)
+			throws JsonParseException, IOException {
+		JsonParser parser = mapper.getFactory().createParser(in);
+		ServiceRequest request = new ServiceRequest();
+
+		JsonToken token = null;
+		String fieldName = null;
+		while ((token = parser.nextToken()) != null) {
+			if (token == JsonToken.FIELD_NAME) {
+				fieldName = parser.getValueAsString();
+			} else if (fieldName != null) {
+				if (fieldName.equals("service")) {
+					ServiceInfo<?> service = parser.readValueAs(ServiceInfo.class);
+					request.setService(service);
+				} else if (fieldName.equals("env")) {
+					EnvironmentInfo env = parser.readValueAs(EnvironmentInfo.class);
+					request.setEnv(env);
+				} else if (fieldName.equals("rid")) {
+					long rid = parser.getLongValue();
+					request.setRequestId(rid);
+				} else if (fieldName.equals("args")) {
+					int idx = 0;
+					List<Object> args = new ArrayList<Object>();
+					while ((token = parser.nextToken()) != JsonToken.END_ARRAY) {
+						args.add(
+								parser.readValueAs(locator.locate(request.getService().getId()).getArgsClspath()[idx]));
+						idx++;
+					}
+					request.setArgs(args.toArray());
+				} else {
+					throw new Error("Unkown Field");
+				}
+				fieldName = null;
+			}
+
+		}
+
+		return request;
+
+	}
+
+}
---
+++ src/test/java/org/lessrpc/serialize/msgpack/MsgPackSerializerTest.java
@@ -0,0 +1,170 @@
+package test.java.org.lessrpc.serialize.msgpack;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.lessrpc.common.info.EnvironmentInfo;
+import org.lessrpc.common.info.SerializationFormat;
+import org.lessrpc.common.info.ServiceDescription;
+import org.lessrpc.common.info.ServiceInfo;
+import org.lessrpc.common.info.ServiceLocator;
+import org.lessrpc.common.info.ServiceProviderInfo;
+import org.lessrpc.common.info.ServiceRequest;
+import org.lessrpc.common.info.ServiceSupportInfo;
+import org.lessrpc.common.info.responses.ExecuteRequestResponse;
+import org.lessrpc.common.info.responses.IntegerResponse;
+import org.lessrpc.common.info.responses.ProviderInfoResponse;
+import org.lessrpc.common.info.responses.ServiceResponse;
+import org.lessrpc.common.info.responses.ServiceSupportResponse;
+import org.lessrpc.common.info.responses.TextResponse;
+import org.lessrpc.common.serializer.Serializer;
+import org.lessrpc.serialize.msgpack.MsgPackSerializer;
+
+public class MsgPackSerializerTest {
+
+	private static ServiceProviderInfo spInfo;
+
+	private static ServiceInfo<Integer> service;
+
+	private static int serverPort = 4040;
+
+	@BeforeClass
+	public static void init() throws Exception {
+		// setting service provider info
+		spInfo = new ServiceProviderInfo("127.0.0.1", serverPort, EnvironmentInfo.currentEnvInfo());
+
+		// setting service
+		service = new ServiceInfo<Integer>("add", 1);
+	}
+
+	@Test
+	public void testSeviceProviderInfoResponse() {
+
+		ProviderInfoResponse response = new ProviderInfoResponse(200,
+				new ServiceProviderInfo("127.0.0.1", 4040, EnvironmentInfo.currentEnvInfo()));
+
+		Serializer serializer = new MsgPackSerializer();
+		ProviderInfoResponse response2 = null;
+		try {
+			response2 = serializer.deserialize(serializer.serialize(response, ProviderInfoResponse.class),
+					ProviderInfoResponse.class);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		assertEquals(response.getStatus(), response2.getStatus());
+
+		assertEquals(response.getContent(), response2.getContent());
+	}
+
+	@Test
+	public void testSupportResponse() {
+
+		ServiceSupportResponse response = new ServiceSupportResponse(200, new ServiceSupportInfo(service, spInfo,
+				new SerializationFormat[] { SerializationFormat.defaultFotmat() }));
+
+		Serializer serializer = new MsgPackSerializer();
+		ServiceSupportResponse response2 = null;
+		try {
+			response2 = serializer.deserialize(serializer.serialize(response, ServiceSupportResponse.class),
+					ServiceSupportResponse.class);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		assertEquals(response.getStatus(), response2.getStatus());
+
+		assertEquals(response.getContent(), response2.getContent());
+	}
+
+	@Test
+	public void testIntegerResponse() {
+
+		IntegerResponse response = new IntegerResponse(200, 1);
+
+		Serializer serializer = new MsgPackSerializer();
+		IntegerResponse response2 = null;
+		try {
+			response2 = serializer.deserialize(serializer.serialize(response, IntegerResponse.class),
+					IntegerResponse.class);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		assertEquals(response.getStatus(), response2.getStatus());
+
+		assertEquals(response.getContent(), response2.getContent());
+	}
+
+	@Test
+	public void testTextResponse() {
+
+		TextResponse response = new TextResponse(200, "test");
+
+		Serializer serializer = new MsgPackSerializer();
+		TextResponse response2 = null;
+		try {
+			response2 = serializer.deserialize(serializer.serialize(response, TextResponse.class), TextResponse.class);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		assertEquals(response.getStatus(), response2.getStatus());
+
+		assertEquals(response.getContent(), response2.getContent());
+	}
+
+	@SuppressWarnings("unchecked")
+	@Test
+	public void testExecuteResponse() {
+
+		ExecuteRequestResponse<ServiceInfo<Integer>> response = new ExecuteRequestResponse<ServiceInfo<Integer>>(200,
+				new ServiceResponse<>(service, service, 10));
+		ServiceDescription<Integer> desc = new ServiceDescription<>(service,
+				new Class[] { Integer.class, String.class, service.getClass() }, ServiceInfo.class);
+
+		Serializer serializer = new MsgPackSerializer();
+		ExecuteRequestResponse<Integer> response2 = null;
+		try {
+			response2 = serializer.deserialize(serializer.serialize(response, ExecuteRequestResponse.class),
+					ExecuteRequestResponse.class,ServiceLocator.create(desc));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		assertEquals(response.getStatus(), response2.getStatus());
+
+		assertEquals(response.getContent(), response2.getContent());
+	}
+
+	@Test
+	public void testServiceRequest() {
+
+		ServiceRequest req = new ServiceRequest(service, EnvironmentInfo.currentEnvInfo(), 1,
+				new Object[] { 1, "asd",service });
+		ServiceDescription<Integer> desc = new ServiceDescription<>(service,
+				new Class[] { Integer.class, String.class, service.getClass() }, Integer.class);
+
+		Serializer serializer = new MsgPackSerializer();
+		ServiceRequest req2 = null;
+		try {
+			req2 = serializer.deserialize(serializer.serialize(req, ServiceRequest.class),
+					ServiceRequest.class, ServiceLocator.create(desc));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		assertEquals(req.getService(), req2.getService());
+
+		assertEquals(req.getArgs().length, req2.getArgs().length);
+		
+		
+		for (int i = 0; i < req.getArgs().length; i++) {
+			assertEquals(req.getArgs()[i], req2.getArgs()[i]);
+		}
+	}
+	
+	
+
+}
